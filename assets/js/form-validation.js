ec426f5c22e035c931389dc5020b003768d5db92
Andriy Ivanyuk <anymacstore@MacBook-Pro-AnyMac.local>
2020-11-11 01:10:50 +0200

    select validation added

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -42,35 +42,34 @@ function checkPhone(event) {
   checkBtnStatus();
 }
 
-function checkBtnStatus() {
-    if (name.value && email.value && phone.value && service.val) {
-        appointBtn.disabled = false;
-      console.log('value yes');
-    } else {
-        appointBtn.disabled = true;
-        console.log('value no');
-    }
-  }
-
 function checkService(node) {
   const formControl = document.getElementsByClassName("form-control")[3];
   const small = formControl.querySelector("small");
   const sel = formControl.querySelector("select");
-  const icon = formControl.querySelectorAll('i')
+  const icon = formControl.querySelectorAll("i");
+
   if (node.value) {
-    sel.classList.add('needed');
-    sel.classList.remove ('error-needed');
+    sel.classList.add("needed");
+    sel.classList.remove("error-needed");
+    formControl.classList.remove('error');
   } else {
-    sel.classList.add('error-needed');
-    small.classList.add('show-err-desc');
+    sel.classList.add("error-needed");
     small.innerText = "Пожалуйста укажите необходимую услугу";
     formControl.className = "form-control error";
-    icon[0].classList.add('error-icon');
-    icon[1].classList.add('error-icon');
-  }  
+    icon[0].classList.add("error-icon");
+    icon[1].classList.add("error-icon");
+    small.classList.add("hide-desc");
+  }
   checkBtnStatus();
 }
 
-
-
+function checkBtnStatus() {
+  if (name.value && email.value && phone.value && service.value) {
+    appointBtn.disabled = false;
+    console.log("value yes");
+  } else {
+    appointBtn.disabled = true;
+    console.log("value no");
+  }
+}
 
