1b9fe9e386b6c92479dbf709fd103659f4ba5a41
Andriy Ivanyuk <anymacstore@MacBook-Pro-AnyMac.local>
2020-11-12 17:18:59 +0200

    header updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
-var backdrop = document.querySelector(".backdrop");
-var toggleButton = document.querySelector(".toggle-button");
-var mobileNav = document.querySelector(".header__shell__mobile-navigation");
-var desktopNav = document.querySelector(
-  ".header-shell__upper-content__navigation-wrapper__main-navigation"
-);
-var sticky = desktopNav.offsetTop;
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
 var advantageLink = document.querySelector(".advantage_link");
 var worksLink = document.querySelector(".works_link");
 var toggleBar = document.querySelector('.toggle-button');
@@ -32,26 +30,25 @@ $(document).ready(function () {
 });
 
 
-toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
 });
 
-backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none";
-  backdrop.style.display = "none";
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
 });
 
-window.onscroll = function () {
-  addStickyNav();
-};
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
 
-function addStickyNav() {
-  if (window.pageYOffset >= sticky) {
-    desktopNav.classList.add("sticky");
-    toggleBar.classList.add("mobile-sticky");
-  } else {
-    desktopNav.classList.remove("sticky");
-    toggleBar.classList.remove("mobile-sticky");
-  }
-}
+
